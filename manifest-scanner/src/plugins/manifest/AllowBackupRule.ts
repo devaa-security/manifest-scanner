1427a85260f87716b9d0ee922d7663af12dac871
Shivasurya <s5sankarapandian@uwaterloo.ca>
2023-05-05 10:25:19 -0400

    added checks to handle empty manifest attributes

--- manifest-scanner/src/plugins/manifest/AllowBackupRule.ts
+++ manifest-scanner/src/plugins/manifest/AllowBackupRule.ts
@@ -15,7 +15,7 @@ export default class AllowBackupRule extends ManifestPlugin {
   run(): void {
     console.log('✅ Running AllowBackupRule')
     const applicationTag = ManifestPlugin.manifestXMLObject.manifest.application
-    if (applicationTag) {
+    if (applicationTag && applicationTag.length > 0 && applicationTag[0].$) {
       const allowBackupAttribute = applicationTag[0].$['android:allowBackup']
 
       if (allowBackupAttribute && allowBackupAttribute === 'true') {
